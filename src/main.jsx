565249dd2f64ba5edc5db09b798d22597a07c5aa
Gideon Onita <onitagideon30@gmail.com>
2025-11-03 18:47:41 +0100

    fix: remove clerk provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import { ClerkProvider } from '@clerk/clerk-react'
 import ErrorBoundary from './components/global/ErrorBoundary'
 import FallbackUI from './components/fallback/FallbackUI'
 import './index.css'
 import App from './App'
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
 createRoot(document.getElementById('root')).render(
   <ErrorBoundary fallback={<FallbackUI />}>
-    {/*  <ClerkProvider publishableKey={PUBLISHABLE_KEY}> */}
     <App />
-    {/*   </ClerkProvider> */}
   </ErrorBoundary>
 )
