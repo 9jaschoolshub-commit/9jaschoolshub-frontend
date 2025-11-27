d6b9a15896e7912df0ebcf894ba695819d88f45d
Christianah Olude <oludechristianah16@gmail.com>
2025-11-27 22:44:35 +0100

    Worked on the navbar components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,31 @@ import Logo from "./Logo";
 import Container from "./Container";
 import Navbar from "./headerComponents/Navbar";
 import { Menu } from "lucide-react";
+import {useState} from "react";
 
 const Header = () => {
+    const [isMobileOpen, setIsMobileOpen] = useState(false);
+
+    const toggleMobile = () => {
+        setIsMobileOpen(!isMobileOpen);
+    };
   return (
     <Container className={`py-3 bg-blue-900`}>
       <header className="flex items-center justify-between">
-        <Menu className="text-white md:hidden " />
+        <button
+            onClick={toggleMobile}
+            className="text-white md:hidden"
+            >
+            {isMobileOpen ? <X size={28} /> : <Menu size={28} />}
+      </button>
         <div className="ml-[12px] md:ml-0 flex-1 md:flex-none">
           <Logo />
         </div>
         <div>
-          <Navbar />
+            <Navbar
+                isMobileOpen={isMobileOpen}
+                closeMobileMenu={() => setIsMobileOpen(false)}
+            />
         </div>
       </header>
     </Container>
--- src/components/headerComponents/Navbar.jsx
+++ src/components/headerComponents/Navbar.jsx
@@ -1,18 +1,36 @@
-import { navLinks } from "../../data/navData";
-import NavLinks from "./NavLinks";
+// <nav className="hidden md:flex flex-1 text-lg gap-10">
+//     {navLinks.map((navLink) => (
+//         <NavLinks key={navLink.name} {...navLink} />
+//     ))}
+// </nav>
 
-const Navbar = () => {
-  return (
-    <nav className="hidden md:flex flex-1 text-lg gap-10">
-      {navLinks.map((navLink) => (
-        <NavLinks key={navLink.name} {...navLink} />
-      ))}
-    </nav>
+import { Link } from 'react-router';
+// import MobileDropdown from "./MobileDropdown";
 
-    /**
-     * Todo
-     * Implement nav section for mobile
-     */
-  );
-};
-export default Navbar;
+const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "Universities", href: "/universities" },
+    { name: "Courses", href: "/courses" },
+];
+
+export default function Navbar({ isMobileOpen, closeMobileMenu  }) {
+    return (
+        <div className="absolute left-0 top-16 w-full bg-white shadow-md z-50">
+            {navLinks.map((navLink) => (
+                <Link
+                    key={navLink.name}
+                    href={navLink.href}
+                    onClick={closeMobileMenu}
+                >
+                    <div className="px-6 py-6 border-b text-lg font-medium text-[#0A0A0A]">
+                        {navLink.name}
+                    </div>
+                </Link>
+            ))}
+
+            {isMobileOpen && (
+                <MobileDropdown closeMenu={closeMobileMenu} />
+            )}
+        </div>
+    );
+}
