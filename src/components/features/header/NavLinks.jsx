dc48ed8d7cc79c49d195eb50ab3771abab85509a
Cyprian Obi <obicyprian180@gmail.com>
2025-11-29 15:15:31 +0100

    Update NavLinks styles for responsive underline effect
    Underline animation and related styles are now applied only on medium screens and above. Also, active link color is changed to orange on small screens for better visibility.

--- src/components/features/header/NavLinks.jsx
+++ src/components/features/header/NavLinks.jsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router-dom";
 const NavLinks = ({ path, name, onClick }) => {
   const getClassName = ({ isActive }) => {
     const baseClasses =
-      "relative after:content-[''] after:absolute after:left-0 after:bottom-[-2px] after:w-0 after:h-[2px] after:bg-orange-500 after:transition-all after:duration-300 hover:after:w-full";
+      "relative md:after:content-[''] md:after:absolute md:after:left-0 md:after:bottom-[-2px] md:after:w-0 md:after:h-[2px] md:after:bg-orange-500 md:after:transition-all md:after:duration-300 md:hover:after:w-full";
     const activeClasses = isActive
-      ? "text-black md:text-white hover:text-gray-200 after:w-full"
+      ? "text-orange-400 md:text-white hover:text-gray-200 md:after:w-full"
       : "text-black md:text-white hover:text-orange-400";
     return `${baseClasses} ${activeClasses}`;
   };
